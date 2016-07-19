f26e6fe5356bd6b4cba0035f2ae95abc986a2cdc
Denys Yefremov <denys.yefremov@logicify.com>
2016-07-19 18:40:41 +0300

    done with filter

--- site/script/app/table/table-component.js
+++ site/script/app/table/table-component.js
@@ -9,6 +9,8 @@ define([
         var ctrl = this;
 
         ctrl.shopItems = apiPart.get();
+        ctrl.sortedItems = ctrl.shopItems;
+
         ctrl.filterObj = {
             name: null,
             price: null,
@@ -17,6 +19,7 @@ define([
 
         ctrl.sort = function(sortBy, order) {
             ctrl.shopItems = _.orderBy(ctrl.shopItems, [sortBy], [order]);
+            ctrl.sortedItems = _.orderBy(ctrl.sortedItems, [sortBy], [order]);
         };
 
         ctrl.filter = function(setter) {
@@ -29,20 +32,18 @@ define([
         };
 
         function useFilter() {
-            ctrl.shopItems = apiPart.get();
-
             if (!_.isUndefined(ctrl.filterObj.name) && !_.isNull(ctrl.filterObj.name)) {
-                ctrl.shopItems = _.filter(ctrl.shopItems, function (item) {
+                ctrl.shopItems = _.filter(ctrl.sortedItems, function (item) {
                     return item.name.indexOf(ctrl.filterObj.name) != -1;
                 });
             }
             if (!_.isUndefined(ctrl.filterObj.price) && !_.isNull(ctrl.filterObj.price) && !_.isNaN(parseFloat(ctrl.filterObj.price))) {
-                ctrl.shopItems = _.filter(ctrl.shopItems, function (item) {
+                ctrl.shopItems = _.filter(ctrl.sortedItems, function (item) {
                     return parseFloat(item.price) === parseFloat(ctrl.filterObj.price);
                 });
             }
             if (!_.isUndefined(ctrl.filterObj.count) && !_.isNull(ctrl.filterObj.count) && !_.isNaN(parseInt(ctrl.filterObj.count))) {
-                ctrl.shopItems = _.filter(ctrl.shopItems, function (item) {
+                ctrl.shopItems = _.filter(ctrl.sortedItems, function (item) {
                     return parseInt(item.count) === parseInt(ctrl.filterObj.count);
                 });
             }
